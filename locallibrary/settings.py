65b9d5db38e725f7710540124c9543fdc8418183
Albertojserr <alberto.j.serrano@gmail.com>
2022-04-24 18:45:04 +0200

    Actualizacion host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-6!#fh57fc&^z+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fast-crag-68568.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
